befa85769f8499f8ddf1d6e310d9480d0ceed279
Inesh-Reddy <ineshreddy007@gmail.com>
2024-03-04 07:04:47 +0530

    implementing loading feature

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,25 @@ import axios from "axios";
 
 
 async function getUserData() {
+  await new Promise((r)=>setTimeout(r,5000));
   const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
 	return response.data;
 }
 
 //async component 
 export default async function Home() {
+
   const userDetails = await getUserData();
   return (
-    <div>
-      Hi There! <br />
-      {userDetails.email} <br />
-      {userDetails.name}
+    <div className="flex flex-col justify-center h-screen">
+        <div className="flex justify-center">
+            <div className="border p-8 rounded">
+                <div>
+                    Name: {userDetails?.name}
+                </div>
+                {userDetails?.email}
+            </div>
+        </div>
     </div>
   );
 }
