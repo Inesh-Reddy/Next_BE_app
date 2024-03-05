ad2af6e0ac548414f1ec463e408f6fe8617f77c2
Inesh-Reddy <ineshreddy007@gmail.com>
2024-03-05 06:52:35 +0530

    implementing prsima for connection pooling

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,13 +1,21 @@
 import { NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
 
 
 
 export async function POST(req: NextRequest){
     //extract the body
     const body = await req.json();
-    console.log(body);
+    
 
     //store the body in database
+    await client.user.create({
+        data:{
+            username: body.username,
+            password: body.password
+        }
+    })
 
     return Response.json({
         message: "You are logged in!"
--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,37 @@
-import Image from "next/image";
-import axios from "axios";
+// import Image from "next/image";
+// import axios from "axios";
 
-
-async function getUserData() {
-  // await new Promise((r)=>setTimeout(r,5000));
-  // const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
-	// return response.data;
-
-  //Ugly,Wrong way of calling BE in same next js 
-  const response = await axios.get("http://localhost:3000/api/user");
-  return response.data;
-}
-
-//async component 
-export default async function Home() {
-
-  const userDetails = await getUserData();
+export default function Home() {
   return (
-    <div className="flex flex-col justify-center h-screen">
-        <div className="flex justify-center">
-            <div className="border p-8 rounded">
-                <div>
-                    Name: {userDetails?.name}
-                </div>
-                Email: {userDetails?.email}
-            </div>
-        </div>
+    <div>
+      hi there
     </div>
   );
 }
+// async function getUserData() {
+//   // await new Promise((r)=>setTimeout(r,5000));
+//   // const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+// 	// return response.data;
+
+//   //Ugly,Wrong way of calling BE in same next js 
+//   const response = await axios.get("http://localhost:3000/api/user");
+//   return response.data;
+// }
+
+// //async component 
+// export default async function Home() {
+
+//   const userDetails = await getUserData();
+//   return (
+//     <div className="flex flex-col justify-center h-screen">
+//         <div className="flex justify-center">
+//             <div className="border p-8 rounded">
+//                 <div>
+//                     Name: {userDetails?.name}
+//                 </div>
+//                 Email: {userDetails?.email}
+//             </div>
+//         </div>
+//     </div>
+//   );
+// }
